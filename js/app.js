b630318b77f8356507760102b304e96f41d3dee3
Njikem <123434988+Njikem@users.noreply.github.com>
2023-03-17 23:59:11 -0600

    arrays

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Creating arrays.
+let item_names = ["cat", "dog", "pig"];
+let item_prices = [25, 50, 65];
+let in_stock = [true, false, true];
+
+// Creating variables and using the push and the pop function.
+first_item_name = item_names[0];
+first_item_price = item_prices[0];
+
+item_names.push("hen");
+item_prices.push(75);
+in_stock.push(false);
+
+ let last_item_names - item_names.pop();
+ let last_item_prices - item_prices.pop();
+ let last_in_stock - in_stock.pop();
+
